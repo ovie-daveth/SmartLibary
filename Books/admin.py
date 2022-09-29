106d8f9e5f8bc7c878966959e1efaa2e04171441
ovie-daveth <105714886+ovie-daveth@users.noreply.github.com>
2022-09-30 00:32:15 +0100

    In progress

--- Books/admin.py
+++ Books/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Books, Genres
 
 # Register your models here.
+admin.site.register(Books)
+admin.site.register(Genres)
--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Userprofile, Nationality
+
+admin.site.register(Userprofile)
+admin.site.register(Nationality)
 
 # Register your models here.
